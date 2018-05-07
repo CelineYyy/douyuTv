020946d8f51497f608b86c70252eff207205737d
365D1 <292018748@qq.com>
2018-05-08 00:10:50 +0800

    2018-05-08

---
+++ DouyuTv/DouyuTv/AppDelegate.swift
@@ -0,0 +1,52 @@
+// +----------------------------------------------------------------------
+// | 广西西途比网络科技有限公司 www.c2b666.com
+// +----------------------------------------------------------------------
+// | 功能描述: AppDelegate.swift
+// +----------------------------------------------------------------------
+// | 时　　间: 2018/5/8
+// +----------------------------------------------------------------------
+// | 代码创建: 朱荻 <292018748@qq.com>
+// +----------------------------------------------------------------------
+// | 版本信息: V1.0.0
+// +----------------------------------------------------------------------
+// | 代码修改:（修改人 - 修改时间）
+// +----------------------------------------------------------------------
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
